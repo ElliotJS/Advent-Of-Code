9c776ad219faeab6b6ed7f2d44003f508ad27025
Elliot Smith <elliot@elliotjs.co.uk>
2023-12-04 10:11:48 +0000

    Day 2 Part 2

--- Day 2/part2.js
+++ Day 2/part2.js
@@ -10,40 +10,46 @@ module.exports = async function processLineByLine() {
   });
 
   const maxNums = [
-    { string: 'red', max: 12 },
-    { string: 'green', max: 13 },
-    { string: 'blue', max: 14 },
+    { string: 'red', lowest: 0 },
+    { string: 'green', lowest: 0 },
+    { string: 'blue', lowest: 0 },
   ];
 
   let total = 0;
 
   for await (const line of rl) {
     const colonIndex = line.indexOf(':');
-    const gameId = Number.parseInt(line.slice(4, colonIndex));
 
     let gameData = line.slice(colonIndex + 1);
     gameData = gameData.split(';');
-    let possible = true;
+
+    let maxNumsTemp = maxNums;
+    let lowestVals = { red: 0, green: 0, blue: 0 };
 
     for (roundData of gameData) {
       const colourDatas = roundData.split(',');
       for (colourData of colourDatas) {
-        for (colour of maxNums) {
+        for (colour of maxNumsTemp) {
           if (colourData.includes(colour.string)) {
-            console.log(`${colourData} - ${colour.max}`);
             const splitBySpaces = colourData.split(' ');
-            if (Number.parseInt(splitBySpaces[1]) > colour.max) {
-              console.log(`Failed ${total}`);
-              possible = false;
+            if (
+              Number.parseInt(splitBySpaces[1]) >
+              lowestVals[colour.string]
+            ) {
+              console.log(`${colourData} - ${colour.max}`);
+              lowestVals[colour.string] = Number.parseInt(
+                splitBySpaces[1]
+              );
             }
           }
         }
       }
     }
-    if (possible) {
-      console.log(`Not Failed ${total}`);
-      total += gameId;
-    }
+
+    let power = lowestVals.red * lowestVals.green * lowestVals.blue;
+    console.log(`${power}`);
+
+    total += power;
   }
   console.log(total);
 };
